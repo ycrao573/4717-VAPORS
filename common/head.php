12c28cb58ec341ccf38a887446337b91da8fba9c
ycrao573 <raoyuchenom@gmail.com>
2020-11-08 15:55:36 +0800

    fix shop.php

--- common/head.php
+++ common/head.php
@@ -3,6 +3,7 @@
     <meta charset="utf-8">
     <link rel="stylesheet" type="text/css" href="./css/style.css">
     <script src='./js/formvalidation.js' type='text/javascript'></script>
+    <script src='./js/product.js' type='text/javascript'></script>
     <meta http-equiv="cache-control" content="no-cache" />
     <meta http-equiv="Pragma" content="no-cache" />
     <meta http-equiv="expires" content="0" />
