35d1e427fae26692d42471d17059b0b031ff8e24
Liangyu <3439388084@qq.com>
2020-11-09 20:34:26 +0800

    update db

--- init.sql
+++ init.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 09, 2020 at 01:07 AM
+-- Generation Time: Nov 09, 2020 at 08:00 PM
 -- Server version: 5.5.44-0ubuntu0.14.04.1
 -- PHP Version: 5.5.9-1ubuntu4.11
 
@@ -67,16 +67,16 @@ CREATE TABLE IF NOT EXISTS `carts` (
   `quantity` int(10) unsigned NOT NULL,
   `paid` int(1) unsigned NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=35 ;
 
 --
 -- Dumping data for table `carts`
 --
 
 INSERT INTO `carts` (`id`, `cartId`, `accountId`, `name`, `category`, `gender`, `color`, `size`, `price`, `discount`, `quantity`, `paid`) VALUES
-(1, 1, 1, 'Nike MAX 2020', 'BRD', 'M', 'White', 9, 129.90, 0.00, 2, 0),
-(2, 2, 1, 'Nike MAX 2020', 'BRD', 'M', 'White', 9, 129.90, 10.00, 2, 0),
-(3, 2, 1, 'Nike ZOOM', 'RUN', 'W', 'White', 9, 129.90, 0.00, 2, 0),
+(1, 1, 1, 'Nike MAX 2020', 'BRD', 'M', 'White', 9, 129.90, 0.00, 2, 1),
+(2, 2, 1, 'Nike MAX 2020', 'BRD', 'M', 'White', 9, 129.90, 10.00, 2, 1),
+(3, 2, 1, 'Nike ZOOM', 'RUN', 'W', 'White', 9, 129.90, 0.00, 2, 1);
 
 -- --------------------------------------------------------
 
@@ -90,6 +90,7 @@ CREATE TABLE IF NOT EXISTS `inventory` (
   `color` varchar(20) NOT NULL,
   `size` varchar(3) NOT NULL,
   `stock` int(10) unsigned NOT NULL,
+  `sale` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `productID` (`productID`,`color`,`size`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=91 ;
@@ -98,85 +99,107 @@ CREATE TABLE IF NOT EXISTS `inventory` (
 -- Dumping data for table `inventory`
 --
 
-INSERT INTO `inventory` (`id`, `productID`, `color`, `size`, `stock`) VALUES
-(1, 1, 'Blue', '7', 0),
-(2, 1, 'Blue', '8', 10),
-(3, 1, 'Blue', '9', 10),
-(4, 1, 'Blue', '10', 180),
-(5, 1, 'Blue', '11', 12),
-(6, 1, 'Blue', '12', 103),
-(7, 1, 'White', '7', 12),
-(8, 1, 'White', '8', 1862),
-(9, 1, 'White', '9', 182),
-(10, 1, 'White', '10', 192),
-(11, 2, 'Brown', '6', 686),
-(12, 2, 'Brown', '7', 686),
-(13, 2, 'Brown', '8', 896),
-(14, 2, 'Brown', '9', 6332),
-(15, 2, 'Brown', '10', 6865),
-(16, 2, 'White', '6', 6526),
-(17, 2, 'White', '7', 638),
-(18, 2, 'White', '8', 6634),
-(19, 2, 'White', '9', 686),
-(20, 2, 'White', '10', 6),
-(21, 3, 'Black', '6', 686),
-(22, 3, 'Black', '7', 686),
-(23, 3, 'Black', '8', 896),
-(24, 3, 'Black', '9', 6332),
-(25, 3, 'Black', '10', 6865),
-(26, 3, 'White', '6', 6526),
-(27, 3, 'White', '7', 638),
-(28, 3, 'White', '8', 6634),
-(29, 3, 'White', '9', 686),
-(30, 3, 'White', '10', 6),
-(31, 4, 'Black', '6', 686),
-(32, 4, 'Black', '7', 686),
-(33, 4, 'Black', '8', 896),
-(34, 4, 'Black', '9', 6333),
-(35, 4, 'Black', '10', 6865),
-(36, 4, 'White', '6', 6536),
-(37, 4, 'White', '7', 638),
-(38, 4, 'White', '8', 6634),
-(39, 4, 'White', '9', 686),
-(40, 4, 'White', '10', 6),
-(41, 5, 'Black', '6', 686),
-(42, 5, 'Black', '7', 686),
-(43, 5, 'Black', '8', 896),
-(44, 5, 'Black', '9', 6333),
-(45, 5, 'Black', '10', 6865),
-(46, 5, 'Orange', '6', 6536),
-(47, 5, 'White', '7', 638),
-(48, 5, 'Orange', '8', 6634),
-(49, 5, 'White', '9', 686),
-(50, 5, 'Orange', '10', 6),
-(51, 6, 'Blue', '6', 686),
-(52, 6, 'Blue', '7', 686),
-(53, 6, 'Blue', '8', 896),
-(54, 6, 'Blue', '9', 6333),
-(55, 6, 'Blue', '10', 6865),
-(57, 6, 'Blue', '11', 638),
-(58, 6, 'Blue', '12', 6634),
-(59, 6, 'Blue', '13', 686),
-(71, 7, 'Brown', '6', 686),
-(72, 7, 'Brown', '7', 686),
-(73, 7, 'Brown', '8', 896),
-(74, 7, 'Brown', '9', 6333),
-(75, 7, 'Brown', '10', 6865),
-(76, 7, 'Pink', '6', 6536),
-(77, 7, 'Pink', '7', 638),
-(78, 7, 'Pink', '8', 6634),
-(79, 7, 'Pink', '9', 686),
-(80, 7, 'Pink', '10', 6),
-(81, 8, 'White', '6', 686),
-(82, 8, 'White', '7', 686),
-(83, 8, 'White', '8', 896),
-(84, 8, 'White', '9', 6333),
-(85, 8, 'White', '10', 6865),
-(86, 8, 'Pink', '6', 6536),
-(87, 8, 'Pink', '7', 638),
-(88, 8, 'Pink', '8', 6634),
-(89, 8, 'Pink', '9', 686),
-(90, 8, 'Pink', '10', 6);
+INSERT INTO `inventory` (`id`, `productID`, `color`, `size`, `stock`, `sale`) VALUES
+(1, 1, 'Blue', '7', 0, 0),
+(2, 1, 'Blue', '8', 10, 0),
+(3, 1, 'Blue', '9', 10, 0),
+(4, 1, 'Blue', '10', 180, 0),
+(5, 1, 'Blue', '11', 12, 0),
+(6, 1, 'Blue', '12', 103, 0),
+(7, 1, 'White', '7', 12, 0),
+(8, 1, 'White', '8', 1862, 0),
+(9, 1, 'White', '9', 182, 0),
+(10, 1, 'White', '10', 192, 0),
+(11, 2, 'Brown', '6', 686, 0),
+(12, 2, 'Brown', '7', 686, 0),
+(13, 2, 'Brown', '8', 896, 0),
+(14, 2, 'Brown', '9', 6332, 0),
+(15, 2, 'Brown', '10', 6865, 0),
+(16, 2, 'White', '6', 6526, 0),
+(17, 2, 'White', '7', 638, 0),
+(18, 2, 'White', '8', 6634, 0),
+(19, 2, 'White', '9', 686, 0),
+(20, 2, 'White', '10', 6, 0),
+(21, 3, 'Black', '6', 686, 0),
+(22, 3, 'Black', '7', 686, 0),
+(23, 3, 'Black', '8', 896, 0),
+(24, 3, 'Black', '9', 6332, 0),
+(25, 3, 'Black', '10', 6865, 0),
+(26, 3, 'White', '6', 6526, 0),
+(27, 3, 'White', '7', 638, 0),
+(28, 3, 'White', '8', 6634, 0),
+(29, 3, 'White', '9', 686, 0),
+(30, 3, 'White', '10', 6, 0),
+(31, 4, 'Black', '6', 686, 0),
+(32, 4, 'Black', '7', 686, 0),
+(33, 4, 'Black', '8', 896, 0),
+(34, 4, 'Black', '9', 6333, 0),
+(35, 4, 'Black', '10', 6865, 0),
+(36, 4, 'White', '6', 6536, 0),
+(37, 4, 'White', '7', 638, 0),
+(38, 4, 'White', '8', 6634, 0),
+(39, 4, 'White', '9', 686, 0),
+(40, 4, 'White', '10', 6, 0),
+(41, 5, 'Black', '6', 686, 0),
+(42, 5, 'Black', '7', 686, 0),
+(43, 5, 'Black', '8', 896, 0),
+(44, 5, 'Black', '9', 6333, 0),
+(45, 5, 'Black', '10', 6865, 0),
+(46, 5, 'Orange', '6', 6536, 0),
+(47, 5, 'White', '7', 638, 0),
+(48, 5, 'Orange', '8', 6634, 0),
+(49, 5, 'White', '9', 686, 0),
+(50, 5, 'Orange', '10', 6, 0),
+(51, 6, 'Blue', '6', 686, 0),
+(52, 6, 'Blue', '7', 686, 0),
+(53, 6, 'Blue', '8', 896, 0),
+(54, 6, 'Blue', '9', 6333, 0),
+(55, 6, 'Blue', '10', 6865, 0),
+(57, 6, 'Blue', '11', 638, 0),
+(58, 6, 'Blue', '12', 6634, 0),
+(59, 6, 'Blue', '13', 686, 0),
+(71, 7, 'Brown', '6', 686, 0),
+(72, 7, 'Brown', '7', 686, 0),
+(73, 7, 'Brown', '8', 896, 0),
+(74, 7, 'Brown', '9', 6333, 0),
+(75, 7, 'Brown', '10', 6865, 0),
+(76, 7, 'Pink', '6', 6536, 0),
+(77, 7, 'Pink', '7', 638, 0),
+(78, 7, 'Pink', '8', 6634, 0),
+(79, 7, 'Pink', '9', 686, 0),
+(80, 7, 'Pink', '10', 6, 0),
+(81, 8, 'White', '6', 686, 0),
+(82, 8, 'White', '7', 686, 0),
+(83, 8, 'White', '8', 896, 0),
+(84, 8, 'White', '9', 6333, 0),
+(85, 8, 'White', '10', 6865, 0),
+(86, 8, 'Pink', '6', 6536, 0),
+(87, 8, 'Pink', '7', 638, 0),
+(88, 8, 'Pink', '8', 6634, 0),
+(89, 8, 'Pink', '9', 686, 0),
+(90, 8, 'Pink', '10', 6, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE IF NOT EXISTS `orders` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `ordersDate` datetime NOT NULL,
+  `accountID` int(10) unsigned NOT NULL,
+  `total` decimal(7,2) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `orders` (`accountID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `ordersDate`, `accountID`, `total`) VALUES
+(1, '2020-11-09 17:37:22', 1, 135.90);
 
 -- --------------------------------------------------------
 
@@ -209,28 +232,6 @@ INSERT INTO `products` (`id`, `name`, `category`, `gender`, `description`, `pric
 (6, 'Air JORDAN M', 'BAS', 'M', 'Beyond capable and controllable.', 399.90, 0.00),
 (7, 'Nike MAX SE', 'CAS', 'M', 'Mate on the ground.', 199.90, 0.00),
 (8, 'Nike ZOOM', 'RUN', 'W', 'Running performance, at joy.', 249.90, 0.00);
-
-
-
---
--- Table structure for table `orders`
---
-
-CREATE TABLE IF NOT EXISTS `orders` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `ordersDate` datetime NOT NULL,
-  `accountID` int(10) unsigned NOT NULL,
-  `total` decimal(7,2) unsigned NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `orders` (`accountID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
---
--- Dumping data for table `orders`
---
-
-INSERT INTO `orders` (`id`, `ordersDate`, `accountID`, `total`) VALUES
-
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
